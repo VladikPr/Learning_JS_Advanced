ebac6f41e29566125e1ee27d422316d23c5abba4
Vladyslav Pryhara <vladikpr94@hotmail.com>
2020-10-13 23:53:57 +0200

    lesson04 New Change

--- js/lesson04.js
+++ js/lesson04.js
@@ -2,9 +2,9 @@ window.addEventListener("DOMContentLoaded", function(){
     
     'use strict';
     
-    let personInput = "88";
+    let personInput = "dasdas sdd";
     function checkType(data){
-        if (isNaN(data)){
+        if (isNaN(data) && data.match(/\d/g) === null){
             let placeholder = data.replace(/ +/g, ' ').trim();
             if (placeholder.length > 30) {
                 return (placeholder.substr(0,29) + "...")
